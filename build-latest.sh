6ef62176fd8a0996ffe100a895c817a2d7ccdee1
Tulio Galli <tg@datapage.com.br>
2025-09-16 14:28:18 -0300

    1.1.3

--- build-latest.sh
+++ build-latest.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-versao='1.1.1'
+versao='1.1.3'
 git add . 
 git commit -m $versao
 git push origin main 
-git tag -a v$versao -m 'adicionando knockd para libeacao'
+git tag -a v$versao -m 'atualizando dependencias e ssl'
 git push origin v$versao
 # git tag -a vlatest -m 'varias libs novas usando lasted '
 # git push origin vlatest
